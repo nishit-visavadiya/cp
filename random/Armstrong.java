dae6b0ef098cdf2566ef8255124d0aa12b8be060
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-07-29 16:18:46 +0530

    initial commit

---
+++ random/Armstrong.java
@@ -0,0 +1,38 @@
+package random;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        Armstrong armstrong = new Armstrong();
+        System.out.println(armstrong.armstrong());
+    }
+
+    boolean armstrong()
+    {
+        int number = getInteger();
+        int original = number;
+        int sum = 0;
+
+        while (number > 0)
+        {
+            int lastDigit = number % 10;
+            sum += Math.pow(lastDigit, 3);
+            number = number / 10;
+        }
+
+        if (original == sum)
+            return true;
+        else
+            return false;
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
+    }
+}
