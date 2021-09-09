9ef8418e1c821320e6699f0895be118cf9ad2db4
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-09 17:19:27 +0530

    initial commit

--- codingbat/string3/SumDigit.java
+++ codingbat/string3/SumDigit.java
@@ -1,4 +1,32 @@
 package codingbat.string3;
 
+import java.util.Scanner;
+
 public class SumDigit {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        SumDigit sumDigit = new SumDigit();
+        sumDigit.execute();
+    }
+
+    void execute()
+    {
+        String s = getString();
+        System.out.println(sumDigit(s));
+    }
+
+    int sumDigit(String str)
+    {
+        //Integer num = Integer.parseInt(str);
+        return Integer.parseInt(str);
+        //return num;
+    }
+
+    String getString()
+    {
+        return sc.nextLine();
+    }
 }
