6b61505043e305984909a1d1b379f50a5842f1d5
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-10 17:23:56 +0530

    initial commit

--- codingbat/array3/CanBalance.java
+++ codingbat/array3/CanBalance.java
@@ -1,4 +1,63 @@
 package codingbat.array3;
 
+import java.util.Scanner;
+
 public class CanBalance {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        CanBalance canBalance = new CanBalance();
+        canBalance.execute();
+    }
+
+    void execute()
+    {
+        int[] arr = setArrayValue();
+        System.out.println(canBalance(arr));
+    }
+
+    private boolean canBalance(int[] nums) {
+        int count = 1, flag = 0;
+        int sum = 0, sum1 = 0;
+        int len = nums.length;
+
+        for (int i = 0; i < count; i++)
+        {
+            sum += nums[i];
+        }
+
+        for (int i = 0; i < len - count; i++)
+        {
+            sum1 += nums[len - 1 - i];
+        }
+        if (sum == sum1)
+        {
+            flag = 1;
+        }
+        count++;
+
+        if (flag == 1)
+            return true;
+
+        return false;
+    }
+
+    int[] setArrayValue()
+    {
+        int sizeOfArray = getInteger();
+        int[] arr = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++)
+        {
+            arr[i] = getInteger();
+        }
+        return arr;
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
+    }
 }
