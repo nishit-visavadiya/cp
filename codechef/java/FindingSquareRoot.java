c3ecce09edd2186616917047828a89d90a980c3d
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-11 12:15:30 +0530

    initial commit

--- codechef/java/FindingSquareRoot.java
+++ codechef/java/FindingSquareRoot.java
@@ -1,4 +1,21 @@
 package codechef.java;
 
+import java.util.Scanner;
+
 public class FindingSquareRoot {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int testCases = sc.nextInt();
+
+        while (testCases --> 0)
+        {
+            int num = sc.nextInt();
+            int squareRoot = (int) Math.sqrt(num);
+            System.out.println(squareRoot);
+        }
+    }
+
 }
