56ee3fd18502340233f4a692db19be2f85b93ba7
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-11 15:13:30 +0530

    initial commit

--- codechef/java/RangeOdd.java
+++ codechef/java/RangeOdd.java
@@ -1,4 +1,20 @@
 package codechef.java;
 
+import java.util.Scanner;
+
 public class RangeOdd {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int left = sc.nextInt();
+        int right = sc.nextInt();
+
+        for (int i = left; i <= right; i++)
+        {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+        }
+    }
 }
