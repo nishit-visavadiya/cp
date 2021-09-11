38c948133b21a30eeb76bbfa8b98efe19dea77b1
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-11 14:53:02 +0530

    initial commit

---
+++ codechef/java/ChefAndRemissness.java
@@ -0,0 +1,30 @@
+package codechef.java;
+
+import java.util.Scanner;
+
+public class ChefAndRemissness {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int testCases = sc.nextInt();
+
+        while (testCases --> 0)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum;
+
+            if (a > b)
+                System.out.print(a + " ");
+            else
+            {
+                System.out.println(b + " ");
+            }
+            sum = a + b;
+            System.out.println(sum);
+        }
+    }
+
+}
