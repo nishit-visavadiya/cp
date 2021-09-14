8625c968878564a703d8e0b823e1087d72b80c6b
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-14 14:22:01 +0530

    initial commit

--- codechef/java/SumOrDifference.java
+++ codechef/java/SumOrDifference.java
@@ -1,4 +1,24 @@
 package codechef.java;
 
+import java.util.Scanner;
+
 public class SumOrDifference {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b)
+        {
+            System.out.print(a - b);
+        }
+        else
+        {
+            System.out.print(a + b);
+        }
+    }
+
 }
