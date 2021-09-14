1f78587947bf74b3fb2d9898bcd0864253ef390a
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-14 15:27:39 +0530

    initial commit

--- codechef/java/IdAndShips.java
+++ codechef/java/IdAndShips.java
@@ -1,4 +1,28 @@
 package codechef.java;
 
+import java.util.Scanner;
+
 public class IdAndShips {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int testCases = sc.nextInt();
+
+        while (testCases -- > 0)
+        {
+            char c = sc.next().charAt(0);
+
+            if (c == 'B' || c == 'b')
+                System.out.println("BattleShip");
+            else if (c == 'C' || c == 'c')
+                System.out.println("Cruiser");
+            else if (c == 'D' || c == 'd')
+                System.out.println("Destroyer");
+            else if (c == 'F' || c == 'f')
+                System.out.println("Frigate");
+        }
+    }
+
 }
