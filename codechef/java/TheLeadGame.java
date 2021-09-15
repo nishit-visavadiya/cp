79433271f6199274f692a17ab64686c1e36171fc
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-15 10:33:12 +0530

    initial commit

--- codechef/java/TheLeadGame.java
+++ codechef/java/TheLeadGame.java
@@ -1,4 +1,45 @@
 package codechef.java;
 
+import java.util.Scanner;
+
 public class TheLeadGame {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum1 = 0, sum2 = 0;
+        int lead = 0, winner = 0;
+        int difference = 0;
+        while (n --> 0)
+        {
+            int playerOne = sc.nextInt();
+            int playerTwo = sc.nextInt();
+
+            sum1 += playerOne;
+            sum2 += playerTwo;
+
+            if (sum1 > sum2)
+            {
+                difference = sum1 - sum2;
+                if (difference > lead)
+                {
+                    lead = difference;
+                    winner = 1;
+                }
+            }
+            else
+            {
+                difference = sum2 - sum1;
+                if (difference >lead)
+                {
+                    lead = difference;
+                    winner = 2;
+                }
+            }
+        }
+        System.out.println(winner + " " + lead);
+    }
+
 }
