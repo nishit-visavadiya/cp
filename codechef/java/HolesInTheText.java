397a281461c8a9ca2d570ae87bd7f14244fc2098
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-15 16:24:44 +0530

    initial commit

---
+++ codechef/java/HolesInTheText.java
@@ -0,0 +1,55 @@
+package codechef.java;
+
+import java.util.Scanner;
+
+public class HolesInTheText {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        HolesInTheText holesInTheText = new HolesInTheText();
+        holesInTheText.execute();
+    }
+
+    void execute()
+    {
+        int testCases = getInteger();
+
+        while (testCases --> 0)
+        {
+            String word = getString();
+            char[] arr = word.toCharArray();
+            System.out.println(countHolesInText(arr));
+        }
+    }
+
+    int countHolesInText(char[] arr)
+    {
+        int count = 0;
+
+        for (char letter : arr)
+        {
+            if (letter == 'A' || letter == 'D' || letter == 'O' || letter == 'P' || letter == 'R')
+            {
+                count++;
+            }
+            if (letter == 'B')
+            {
+                count += 2;
+            }
+        }
+
+        return count;
+    }
+
+    String getString()
+    {
+        return sc.next();
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
+    }
+}
