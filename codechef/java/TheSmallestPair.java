43fb7be5a2b8e0d57ab2872bb8b521b551b5045e
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-16 12:17:24 +0530

    initial commit

---
+++ codechef/java/TheSmallestPair.java
@@ -0,0 +1,72 @@
+package codechef.java;
+
+import java.util.Scanner;
+
+public class TheSmallestPair {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        TheSmallestPair theSmallestPair = new TheSmallestPair();
+        theSmallestPair.execute();
+    }
+
+    void execute()
+    {
+        int testCases = getInteger();
+
+        while (testCases --> 0)
+        {
+            int numberOfArray = getInteger();
+
+            int[] arr = getArray(numberOfArray);
+
+            System.out.println(sumOfFirstTwoMinimumNumberInArray(arr));
+        }
+    }
+
+//    int getSmallestPair(int[] arr)
+//    {
+//        return sumOfFirstTwoMinimumNumberInArray(arr);
+//    }
+
+    int sumOfFirstTwoMinimumNumberInArray(int[] arr)
+    {
+        int minimumElement = 0;
+        int secondMinimum = minimumElement;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < minimumElement)
+            {
+                secondMinimum = minimumElement;
+                minimumElement = arr[i];
+            }
+            else
+            {
+                if (arr[i] < secondMinimum)
+                    secondMinimum = arr[i];
+            }
+        }
+
+        return minimumElement + secondMinimum;
+    }
+
+    int[] getArray(int size)
+    {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = getInteger();
+        }
+
+        return arr;
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
+    }
+
+}
