14cba1cd9bcf7812e12ea1e17dc49fa7e4467371
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-09-17 12:58:09 +0530

    initial commit

---
+++ codechef/java/Rectangle.java
@@ -0,0 +1,46 @@
+package codechef.java;
+
+import java.util.Scanner;
+
+public class Rectangle {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.execute();
+    }
+
+    void execute()
+    {
+        int testCases = getInteger();
+
+        while (testCases --> 0)
+        {
+            int a = getInteger();
+            int b = getInteger();
+            int c = getInteger();
+            int d = getInteger();
+
+            if (isRectangle(a, b, c, d))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+
+    boolean isRectangle(int a, int b, int c, int d)
+    {
+        if ((a == b && c == d) || (a == c && b == d) || (a == d && b == c))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
+    }
+}
