f0fa2cf393fa716cbc54b3f6af1d7047fd5836f5
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-10-12 15:01:09 +0530

    initial commit

---
+++ codechef/java/ChefAndOperators.java
@@ -0,0 +1,59 @@
+package codechef.java;
+
+import java.util.Scanner;
+
+public class ChefAndOperators {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int A,B;
+
+    public static void main(String[] args)
+    {
+        ChefAndOperators chefAndOperators = new ChefAndOperators();
+        chefAndOperators.executeTestCases();
+    }
+
+    void executeTestCases()
+    {
+        int testCases = getInteger();
+
+        while (testCases --> 0)
+        {
+            A = getInteger();
+            B = getInteger();
+
+            System.out.println(findRelationShipBetweenAB());
+        }
+    }
+
+    String findRelationShipBetweenAB()
+    {
+        if (aIsLessThanB())
+            return "<";
+        else if (aIsGreaterThanB())
+            return ">";
+
+        return "=";
+    }
+
+    boolean aIsLessThanB()
+    {
+        return A < B;
+    }
+
+    boolean aIsGreaterThanB()
+    {
+        return A > B;
+    }
+
+    boolean bothAreEqual()
+    {
+        return A == B;
+    }
+
+    int getInteger()
+    {
+        return scanner.nextInt();
+    }
+}
