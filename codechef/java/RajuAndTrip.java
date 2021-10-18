8227de7987cb12ac39e771f77d1023639d0a2eb4
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-10-18 14:55:12 +0530

    initial commit

--- codechef/java/RajuAndTrip.java
+++ codechef/java/RajuAndTrip.java
@@ -4,16 +4,37 @@ import java.util.Scanner;
 
 public class RajuAndTrip {
 
+    Scanner sc = new Scanner(System.in);
+
+    int number;
+
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        if (n % 5 == 0 || n % 6 == 0)
-            System.out.print("YES");
-        else
-            System.out.print("NO");
+        RajuAndTrip rajuAndTrip = new RajuAndTrip();
+        rajuAndTrip.execute();
     }
 
+    void execute()
+    {
+        number = getInteger();
+
+        printAnswer();
+    }
+
+    void printAnswer()
+    {
+        if (isNumberMultipleOfFiveOrSix())
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+
+    boolean isNumberMultipleOfFiveOrSix()
+    {
+        return number % 5 == 0 || number % 6 == 0;
+    }
+
+    int getInteger() {
+        return sc.nextInt();
+    }
 }
