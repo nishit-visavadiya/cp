3e61209839c8bd1dea76c8dfbf4a2fa6f106ce49
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-10-18 14:55:28 +0530

    initial commit

--- codechef/java/PuppyAndSum.java
+++ codechef/java/PuppyAndSum.java
@@ -4,26 +4,54 @@ import java.util.Scanner;
 
 public class PuppyAndSum {
 
+    Scanner sc = new Scanner(System.in);
+
+    int dTime, numbersToSum, sum = 0;
+
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        PuppyAndSum puppyAndSum = new PuppyAndSum();
+        puppyAndSum.executeTestCases();
+    }
 
-        int testCases = sc.nextInt();
+    void executeTestCases()
+    {
+        int testCases = getInteger();
 
-        while (testCases > 0)
+        while (testCases --> 0)
         {
-            int sum = 0;
-            int d = sc.nextInt();
-            int n = sc.nextInt();
+            sum = 0;
 
-            while (d != 0)
-            {
-                sum = (n * (n + 1)) / 2;
-                n = sum;
-                d--;
-            }
-            System.out.println(sum);
-            testCases--;
+            dTime = getInteger();
+            numbersToSum = getInteger();
+
+            System.out.println(calculateSumOfIntegers());
         }
+    }
+
+    int calculateSumOfIntegers()
+    {
+        while (dIsNotZero())
+        {
+            sum = sumOfNNumbers();
+            numbersToSum = sum;
+            dTime--;
+        }
+        return sum;
+    }
+
+    int sumOfNNumbers()
+    {
+        return (numbersToSum * (numbersToSum + 1)) / 2;
+    }
+
+    boolean dIsNotZero()
+    {
+        return dTime > 0;
+    }
+
+    int getInteger()
+    {
+        return sc.nextInt();
     }
 }
